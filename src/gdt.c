08ab76b7fa5876ee41bffa657744254382339a0f
Ivan <ivercinsky@gmail.com>
2017-10-17 20:31:38 -0300

    primer commit

---
+++ src/gdt.c
@@ -0,0 +1,118 @@
+/* ** por compatibilidad se omiten tildes **
+================================================================================
+ TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+================================================================================
+  definicion de la tabla de descriptores globales
+*/
+
+#include "gdt.h"
+
+
+
+
+/* Definicion de la GDT */
+/* -------------------------------------------------------------------------- */
+
+gdt_entry gdt[GDT_COUNT] = {
+    /* Descriptor nulo*/
+    /* Offset = 0x00 */
+    [GDT_IDX_NULL_DESC] = (gdt_entry) {
+        (unsigned short)    0x0000,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x00,           /* type         */
+        (unsigned char)     0x00,           /* s            */
+        (unsigned char)     0x00,           /* dpl          */
+        (unsigned char)     0x00,           /* p            */
+        (unsigned char)     0x00,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x00,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+
+    //limite del segmento 500MB, o sea de 0 a 500MB - 1  si 500MB es 0x1f400000, el limite es 1f3fffff
+    [GDT_IDX_CODE_0] = (gdt_entry) {
+        (unsigned short)    0xf3ff,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x0A,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x00,           /* dpl          */
+        (unsigned char)     0x01,           /* p            */
+        (unsigned char)     0x01,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+    [GDT_IDX_CODE_3] = (gdt_entry) {
+        (unsigned short)    0xf3ff,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x0A,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x03,           /* dpl          */
+        (unsigned char)     0x00,           /* p            */
+        (unsigned char)     0x01,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+    [GDT_IDX_DATA_0] = (gdt_entry) {
+        (unsigned short)    0xf3ff,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x02,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x00,           /* dpl          */
+        (unsigned char)     0x01,           /* p            */
+        (unsigned char)     0x01,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+    [GDT_IDX_DATA_3] = (gdt_entry) {
+        (unsigned short)    0xf3ff,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x02,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x03,           /* dpl          */
+        (unsigned char)     0x01,           /* p            */
+        (unsigned char)     0x01,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+    [GDT_IDX_VIDEO] = (gdt_entry) {
+        (unsigned short)    0x2000,         /* limit[0:15]  */
+        (unsigned short)    0x8000,         /* base[0:15]   */
+        (unsigned char)     0x0B,           /* base[23:16]  */
+        (unsigned char)     0x02,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x03,           /* dpl          */
+        (unsigned char)     0x01,           /* p            */
+        (unsigned char)     0x00,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x00,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    }
+
+
+};
+
+gdt_descriptor GDT_DESC = {
+    sizeof(gdt) - 1,
+    (unsigned int) &gdt
+};
---
+++ src/utils.asm
@@ -0,0 +1,9 @@
+
+
+
+;; DEFINES DE DESCRIPTORES DE SEGMENTOS
+;%define NOMBRE_DEL_DEFINE ((INDICE<<3) | (TABLE_INDICATOR<<2) | RPL)
+%define CODE_0 ((8<<3) | (0<<2) | 0)
+%define CODE_3 ((11<<3) | (0<<2) | 3)
+%define DATA_0 ((10<<3) | (0<<2) | 0)
+%define VIDEO ((12<<3) | (0<<2) | 0)
